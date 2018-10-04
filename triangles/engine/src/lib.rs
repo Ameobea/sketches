c42c07935a4938bd2838a5d4cc3c02af5688bc05
Casey Primozic <me@ameo.link>
2018-10-04 13:44:50 -0500

    Set up interactivity + fix triangle positioning
     * Triangles are placed at 60 degree rotations to the point that they're being placed at, either positive or negative depending on the point.
     * Install React and `react-control-panel` and hook it up to control the settings of the underlying sketch dynamically
       * Pull in `serde` and friends to parse the setting object JSON and re-render (after clearing the canvas) every time config is changed

--- triangles/engine/src/lib.rs
+++ triangles/engine/src/lib.rs
@@ -1,4 +1,4 @@
-#![feature(box_syntax)]
+#![feature(box_syntax, const_fn)]
 
 extern crate common;
 extern crate wasm_bindgen;
@@ -6,6 +6,10 @@ extern crate wasm_bindgen;
 extern crate lazy_static;
 extern crate nalgebra;
 extern crate ncollide2d;
+extern crate serde;
+extern crate serde_json;
+#[macro_use]
+extern crate serde_derive;
 
 use std::f32;
 use std::ptr;
@@ -19,36 +23,47 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen(module = "./index")]
 extern "C" {
-    pub fn render_triangle(x1: f32, y1: f32, x2: f32, y2: f32, x3: f32, y3: f32, color: &str);
+    pub fn render_triangle(
+        x1: f32,
+        y1: f32,
+        x2: f32,
+        y2: f32,
+        x3: f32,
+        y3: f32,
+        color: &str,
+        border_color: &str,
+    );
 }
 
 type TriangleBuf = [Point2<f32>; 3];
 
-const CANVAS_HEIGHT: usize = 800;
-const CANVAS_WIDTH: usize = 800;
-const TRIANGLE_SIZE: f32 = 7.0;
-const TRIANGLE_OFFSET_X: f32 = TRIANGLE_SIZE / 2.0;
-const TRIANGLE_COUNT: usize = 200;
-const MAX_ROTATION_RADS: f32 = 0.;
+const RADS_PER_CIRCLE: f32 = f32::consts::PI * 2.0;
 
-lazy_static! {
-    static ref TRIANGLE_OFFSET_Y: f32 =
-        ((TRIANGLE_SIZE * TRIANGLE_SIZE) - (TRIANGLE_OFFSET_X * TRIANGLE_OFFSET_X)).sqrt();
-    static ref TRIANGLE_COORDS: TriangleBuf = [
-        Point2::new(0.0, 0.0),
-        Point2::new(-TRIANGLE_OFFSET_X, *TRIANGLE_OFFSET_Y),
-        Point2::new(TRIANGLE_OFFSET_X, *TRIANGLE_OFFSET_Y),
-    ];
+#[derive(Deserialize)]
+pub struct Conf<'a> {
+    pub canvas_width: usize,
+    pub canvas_height: usize,
+    pub triangle_size: f32,
+    pub triangle_count: usize,
+    pub max_rotation_rads: f32,
+    pub triangle_color: &'a str,
+    pub triangle_border_color: &'a str,
 }
 
-fn render_triangle_array(transformed: &mut TriangleBuf, offset: Isometry2<f32>, color: &str) {
+fn render_triangle_array(
+    base_triangle: &TriangleBuf,
+    transformed: &mut TriangleBuf,
+    offset: Isometry2<f32>,
+    color: &str,
+    border_color: &str,
+) {
     let new_triangle = [
-        offset * (*TRIANGLE_COORDS)[0],
-        offset * (*TRIANGLE_COORDS)[1],
-        offset * (*TRIANGLE_COORDS)[2],
+        offset * base_triangle[0],
+        offset * base_triangle[1],
+        offset * base_triangle[2],
     ];
     let [p1, p2, p3] = new_triangle;
-    render_triangle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, color);
+    render_triangle(p1.x, p1.y, p2.x, p2.y, p3.x, p3.y, color, border_color);
     *transformed = new_triangle
 }
 
@@ -64,38 +79,78 @@ pub fn init() {
     }
 }
 
-#[wasm_bindgen]
-pub fn render() {
-    let world: &mut World = unsafe { &mut *COLLISION_WORLD };
+const fn deg_to_rad(degrees: f32) -> f32 {
+    degrees * (f32::consts::PI / 180.0)
+}
 
-    let color = "#ab13dc";
-    let mut last_triangle = *TRIANGLE_COORDS;
+#[wasm_bindgen]
+pub fn render(conf_str: &str) {
+    let world: &mut World = unsafe { &mut *COLLISION_WORLD };
+    *world = DBVT::new();
+    let Conf {
+        canvas_width,
+        canvas_height,
+        triangle_size,
+        triangle_count,
+        max_rotation_rads,
+        triangle_color,
+        triangle_border_color,
+    } = match serde_json::from_str(conf_str) {
+        Ok(conf) => conf,
+        Err(err) => {
+            common::error(format!("Error decoding provided conf object: {:?}", err));
+            return;
+        }
+    };
+    let triangle_offset_x = triangle_size / 2.0;
+    let triangle_offset_y =
+        ((triangle_size * triangle_size) - (triangle_offset_x * triangle_offset_x)).sqrt();;
+    let base_triangle_coords: TriangleBuf = [
+        Point2::new(0.0, 0.0),
+        Point2::new(-triangle_offset_x, triangle_offset_y),
+        Point2::new(triangle_offset_x, triangle_offset_y),
+    ];
+
+    let mut last_triangle = base_triangle_coords;
     let mut rotation = 0.0;
     render_triangle_array(
+        &base_triangle_coords,
         &mut last_triangle,
-        Isometry2::new(Vector2::new(400.0, 400.0), rotation),
-        color,
+        Isometry2::new(
+            Vector2::new(canvas_width as f32 / 2.0, canvas_height as f32 / 2.0),
+            rotation,
+        ),
+        triangle_color,
+        triangle_border_color,
     );
     let shape = Triangle::new(last_triangle[0], last_triangle[1], last_triangle[2]);
-    world.insert(DBVTLeaf::new(
-        shape.to_polyline(0.0).aabb(&Isometry2::identity()),
-        (),
-    ));
+    // world.insert(DBVTLeaf::new(
+    //     shape.to_polyline(()).aabb(&Isometry2::identity()),
+    //     (),
+    // ));
 
-    for _ in 0..TRIANGLE_COUNT {
+    for _ in 0..triangle_count {
         // pick one of the other two vertices to use as the new origin
         let (ix, rot_offset) = if common::math_random() > 0.5 {
-            (1, 1. * (2.0 * f32::consts::PI) / 3.)
+            (1, deg_to_rad(60.0))
         } else {
-            (2, 2. * (2.0 * f32::consts::PI) / 3.)
+            (2, deg_to_rad(-60.0))
         };
         let origin = last_triangle[ix];
         // set a random rotation for now
-        rotation += rot_offset + (common::math_random() as f32 - 0.5) * 2. * MAX_ROTATION_RADS;
+        rotation += rot_offset;
+        rotation += (common::math_random() as f32 - 0.5) * 2. * max_rotation_rads;
+        let base = (rotation / RADS_PER_CIRCLE).trunc();
+        common::log(format!("{:?}", rotation - base * RADS_PER_CIRCLE));
         render_triangle_array(
+            &base_triangle_coords,
             &mut last_triangle,
-            Isometry2::new(Vector2::new(origin[0], origin[1]), rotation),
-            color,
+            Isometry2::new(
+                Vector2::new(origin[0], origin[1]),
+                rotation - base * RADS_PER_CIRCLE,
+            ),
+            triangle_color,
+            triangle_border_color,
         )
     }
 }
---
+++ triangles/src/index.tsx
@@ -0,0 +1,76 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Panel from 'react-control-panel';
+
+const wasm = import('./engine');
+
+const SVG: HTMLElement = document.getElementById('svg') as any;
+
+export const render_triangle = (
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number,
+  x3: number,
+  y3: number,
+  color: string,
+  border_color: string
+) => {
+  const poly: SVGPolygonElement = (document.createElementNS(
+    'http://www.w3.org/2000/svg',
+    'polygon'
+  ) as any) as SVGPolygonElement;
+  poly.setAttribute('points', `${x1},${y1} ${x2},${y2} ${x3},${y3}`);
+  poly.setAttribute('style', `fill:${color};stroke:${border_color};stroke-width:1`);
+  SVG.appendChild(poly);
+};
+
+const deleteAllChildren = (node: HTMLElement) => {
+  while (node.firstChild) {
+    node.removeChild(node.firstChild);
+  }
+};
+
+wasm.then(engine => {
+  engine.init();
+
+  const App = () => (
+    <Panel
+      position="top-right"
+      title="Sketch Config"
+      settings={settings}
+      onChange={(_key, _val, state) => {
+        SVG.setAttribute('height', state.canvas_height);
+        SVG.setAttribute('width', state.canvas_width);
+        deleteAllChildren(SVG);
+
+        engine.render(
+          JSON.stringify({ ...state, triangle_count: Math.round(state.triangle_count) })
+        );
+      }}
+      width={500}
+    />
+  );
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<App />, root);
+});
+
+const settings = [
+  { type: 'range', label: 'prng_seed', min: 0, max: 1, steps: 1000 },
+  { type: 'range', label: 'canvas_width', min: 100, max: 1000, initial: 800 },
+  { type: 'range', label: 'canvas_height', min: 100, max: 1000, initial: 800 },
+  { type: 'range', label: 'triangle_size', min: 1.0, max: 50.0, step: 0.5, initial: 10.0 },
+  { type: 'color', label: 'triangle_border_color', initial: '#7c007c', format: 'rgb' },
+  { type: 'color', label: 'triangle_color', initial: 'rgb(81, 12, 84)', format: 'rgb' },
+  {
+    type: 'range',
+    label: 'triangle_count',
+    initial: 200,
+    min: 1,
+    max: 10000,
+    steps: 100,
+    scale: 'log',
+  },
+  { type: 'range', label: 'max_rotation_rads', initial: 0.5, min: 0.0, max: Math.PI },
+];
